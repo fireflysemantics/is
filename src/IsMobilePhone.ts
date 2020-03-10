db5283404c2c6a49e18bbf7fccf0eff1d3a67d50
Ole Ersoy <ole.ersoy@gmail.com>
2020-03-10 01:12:46 -0500

    Reexported MobilePhoneLocal

--- src/IsMobilePhone.ts
+++ src/IsMobilePhone.ts
@@ -1,4 +1,5 @@
-import { isMobilePhoneLocales as vtsIsMobilePhoneLocales, IsMobilePhoneOptions as vtsIsMobilePhoneOptions, isMobilePhone as vtsIsMobilePhone } from "@fireflysemantics/validatorts";
+import { isMobilePhoneLocales as vtsIsMobilePhoneLocales, IsMobilePhoneOptions as vtsIsMobilePhoneOptions, isMobilePhone as vtsIsMobilePhone } from "@fireflysemantics/validatorts"
+export { MobilePhoneLocale } from "@fireflysemantics/validatorts"
 
 /**
  * Checks if the string is a mobile phone number (locale is one of ['zh-CN', 'zh-TW', 'en-ZA', 'en-AU', 'en-HK',
@@ -10,7 +11,7 @@ import { isMobilePhoneLocales as vtsIsMobilePhoneLocales, IsMobilePhoneOptions a
  * @return True if the string is a mobile phone number, false otherwise.
  */
 export function isMobilePhone(value: string, locale: string, options?: IsMobilePhoneOptions): boolean {
-    return (typeof value === "string" && vtsIsMobilePhone(value, locale, options));
+    return ( typeof value === "string" && vtsIsMobilePhone(value, locale, options));
 }
 
 /**
