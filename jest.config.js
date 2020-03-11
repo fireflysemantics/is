35fe607bfeaf5809ae490dfb4b813ce67e6a951c
Ole Ersoy <ole.ersoy@gmail.com>
2020-03-10 20:40:34 -0500

    New Angular Package Format Content

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  roots: ["./src"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  moduleNameMapper: {
-    "^@fs/(.*)$": "<rootDir>/src/$1"
+  "roots": [
+    "<rootDir>/projects/is/src/lib/"
+  ],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
   }
-};
+}
